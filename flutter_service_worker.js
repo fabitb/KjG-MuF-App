64c104a1e5c7413edbe340dd02d9d60d37236486
fabitb <fabian.thomas-barein@tum.de>
2023-07-12 05:20:31 +0000

    Deploying to gh-pages from @ fabitb/KjG-MuF-App@05fff3f421f646650eaa26b8c5dbc70967698c3c 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "0761d10a51888ca8023c46ae3c6fab39",
-"/": "0761d10a51888ca8023c46ae3c6fab39",
+const RESOURCES = {"index.html": "8cfea1f33558571486a9a385a57575cc",
+"/": "8cfea1f33558571486a9a385a57575cc",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "8215070bdfc3b3865e053a7dc5c6fc5a",
+"main.dart.js": "f8371b36c08646eca50cb377c755dffa",
 "version.json": "ca7f8fd6f325cd1cc4a4f188b79b013b",
 "canvaskit/chromium/canvaskit.js": "8c8392ce4a4364cbb240aa09b5652e05",
 "canvaskit/chromium/canvaskit.wasm": "fc18c3010856029414b70cae1afc5cd9",
