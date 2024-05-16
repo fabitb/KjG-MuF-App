1d9071fea9d93b12ba7fd33b4e88a6c67fa07129
Oliver Schneider <schneider@mobile-software.ag>
2024-05-16 22:37:17 +0200

    fix: remove autocorrect from username input

--- lib/ui/screens/login_screen.dart
+++ lib/ui/screens/login_screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:kjg_muf_app/constants/strings.dart';
 import 'package:provider/provider.dart';
 
-import '../../viewmodels/main.viewmodel.dart';
+import 'package:kjg_muf_app/viewmodels/main.viewmodel.dart';
 
 class LoginScreen extends StatelessWidget {
   LoginScreen({super.key});
@@ -16,33 +16,42 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (_) => MainViewModel(),
-      child: Consumer<MainViewModel>(builder: (_, model, __) {
-        return Scaffold(
-          resizeToAvoidBottomInset: false,
-          appBar: AppBar(
-            title: const Text("Einloggen"),
-          ),
-          body: Center(
-            child: Padding(
+      child: Consumer<MainViewModel>(
+        builder: (_, model, __) {
+          return Scaffold(
+            resizeToAvoidBottomInset: false,
+            appBar: AppBar(
+              title: const Text("Einloggen"),
+            ),
+            body: Center(
+              child: Padding(
                 padding:
                     const EdgeInsets.symmetric(horizontal: 8.0, vertical: 64.0),
                 child: !model.isLoggedIn
                     ? Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: <Widget>[
-                          const Text("Login Mida",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 24)),
+                          const Text(
+                            "Login Mida",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24,
+                            ),
+                          ),
                           const SizedBox(height: 50),
                           TextField(
                             controller: emailController,
                             style: const TextStyle(color: Colors.black),
+                            autocorrect: false,
+                            enableSuggestions: false,
                             decoration: InputDecoration(
-                                fillColor: Colors.grey.shade100,
-                                filled: true,
-                                hintText: "Benutzername",
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(10))),
+                              fillColor: Colors.grey.shade100,
+                              filled: true,
+                              hintText: "Benutzername",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
                           ),
                           const SizedBox(height: 10),
                           TextField(
@@ -50,43 +59,55 @@ class LoginScreen extends StatelessWidget {
                             style: const TextStyle(color: Colors.black),
                             obscureText: true,
                             decoration: InputDecoration(
-                                fillColor: Colors.grey.shade100,
-                                filled: true,
-                                hintText: "Password",
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(10))),
+                              fillColor: Colors.grey.shade100,
+                              filled: true,
+                              hintText: "Password",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
                           ),
                           const SizedBox(height: 50),
                           !model.loading
                               ? ElevatedButton(
                                   onPressed: () {
-                                    model.login(emailController.text,
-                                        passwordController.text);
+                                    model.login(
+                                      emailController.text,
+                                      passwordController.text,
+                                    );
                                   },
                                   style: ElevatedButton.styleFrom(elevation: 5),
                                   child: const Text("Einloggen"),
                                 )
                               : const Center(
-                                  child: CircularProgressIndicator()),
+                                  child: CircularProgressIndicator(),
+                                ),
                           if (!Platform.isIOS) ...[
                             const Expanded(
-                                child: Align(
-                                    alignment: Alignment.bottomCenter,
-                                    child: Text(
-                                        "Du hast noch keinen Mida Zugang oder willst KjG Mitglied werden?",
-                                        textAlign: TextAlign.center))),
+                              child: Align(
+                                alignment: Alignment.bottomCenter,
+                                child: Text(
+                                  "Du hast noch keinen Mida Zugang oder willst KjG Mitglied werden?",
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ),
                             const SizedBox(height: 16),
                             ElevatedButton(
-                                onPressed: () => model.openUrl(
-                                    "mailto:${Strings.contactEmailAddress}"),
-                                child: const Text("Schreib uns eine Mail!"))
-                          ]
+                              onPressed: () => model.openUrl(
+                                "mailto:${Strings.contactEmailAddress}",
+                              ),
+                              child: const Text("Schreib uns eine Mail!"),
+                            ),
+                          ],
                         ],
                       )
-                    : Text("Du bist eingeloggt ${model.nameCache}")),
-          ),
-        );
-      }),
+                    : Text("Du bist eingeloggt ${model.nameCache}"),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
