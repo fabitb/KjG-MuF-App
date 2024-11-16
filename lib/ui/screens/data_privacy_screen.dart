e25364e8956b04121bb15d834e0754cb38783769
Oliver Schneider <schneider@mobile-software.ag>
2024-11-16 18:41:10 +0100

    update packages

--- lib/ui/screens/data_privacy_screen.dart
+++ lib/ui/screens/data_privacy_screen.dart
@@ -3,18 +3,7 @@ import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:kjg_muf_app/constants/strings.dart';
 
 class DataPrivacyScreen extends StatelessWidget {
-  DataPrivacyScreen({super.key});
-
-  final InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
-      crossPlatform: InAppWebViewOptions(
-        useShouldOverrideUrlLoading: true,
-      ),
-      android: AndroidInAppWebViewOptions(
-        useHybridComposition: true,
-      ),
-      ios: IOSInAppWebViewOptions(
-        allowsInlineMediaPlayback: true,
-      ));
+  const DataPrivacyScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -23,16 +12,38 @@ class DataPrivacyScreen extends StatelessWidget {
         title: const Text("Datenschutz"),
       ),
       body: InAppWebView(
-          initialOptions: options,
-          shouldOverrideUrlLoading: (controller, navigationAction) async {
-            if (navigationAction.request.url.toString() ==
-                Strings.dataPrivacyLink) {
-              return NavigationActionPolicy.ALLOW;
-            }
-            return NavigationActionPolicy.CANCEL;
-          },
-          initialUrlRequest:
-              URLRequest(url: Uri.parse(Strings.dataPrivacyLink))),
+        initialSettings: InAppWebViewSettings(
+          useShouldOverrideUrlLoading: true,
+        ),
+        onPageCommitVisible: (controller, url) {
+          controller.addUserScript(
+            userScript: UserScript(
+              source: """
+                    var header = document.querySelector('div.container.main-container');
+                    var menuButton = document.querySelector('.rmp_menu_trigger');
+                    var crumbs = document.querySelector('.breadcrumbs');
+                    var bottomMenu = document.querySelector('#colophon');
+                    var body = document.querySelector('body');
+                    body.classList.remove("has-no-thumb");
+                    header.remove();
+                    menuButton.remove();
+                    crumbs.remove();
+                    bottomMenu.remove();
+                    """,
+              injectionTime: UserScriptInjectionTime.AT_DOCUMENT_END,
+            ),
+          );
+        },
+        shouldOverrideUrlLoading: (controller, navigationAction) async {
+          if (navigationAction.request.url.toString() ==
+              Strings.dataPrivacyLink) {
+            return NavigationActionPolicy.ALLOW;
+          }
+          return NavigationActionPolicy.CANCEL;
+        },
+        initialUrlRequest:
+            URLRequest(url: WebUri.uri(Uri.parse(Strings.dataPrivacyLink))),
+      ),
     );
   }
 }
