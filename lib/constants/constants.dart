28d55c7564b176f6c9a47e74d6e850eb9d887de8
Fabian Thomas-Barein <fthomas-barein@kjg-muenchen.de>
2025-01-05 14:34:19 +0100

    KJG-105: add parsing for ordernumber and sorting dashboard

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,3 +1,4 @@
 abstract class Constants {
   static const double phoneMaxWidth = 600;
+  static const int maxInt = -1 >>> 1;
 }
--- lib/model/news.dart
+++ lib/model/news.dart
@@ -1,14 +1,18 @@
+import 'package:kjg_muf_app/constants/constants.dart';
+
 class News {
   final String title;
   final String content;
   final String imageURL;
   final String websiteURL;
+  final int orderNumber;
 
   const News({
     required this.title,
     required this.content,
     required this.imageURL,
     required this.websiteURL,
+    required this.orderNumber,
   });
 
   factory News.fromJson(Map<String, dynamic> json) {
@@ -17,12 +21,14 @@ class News {
     String content = yoastContent["og_description"];
     String imageURL = yoastContent["og_image"][0]["url"];
     String websiteURL = yoastContent["og_url"];
+    int orderNumber = json["menu_order"] ?? Constants.maxInt;
 
     return News(
       title: title,
       content: content,
       imageURL: imageURL,
       websiteURL: websiteURL,
+      orderNumber: orderNumber,
     );
   }
 
@@ -34,6 +40,7 @@ class News {
         content: "NewsContent",
         imageURL: "",
         websiteURL: "NewsWebsiteURL",
+        orderNumber: 0,
       ),
     );
   }
