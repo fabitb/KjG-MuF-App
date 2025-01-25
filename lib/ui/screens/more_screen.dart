848261a0cb8c5bd59317bdb320b4181ff6ff99e2
Fabian Thomas-Barein <fthomas-barein@kjg-muenchen.de>
2025-01-25 17:56:07 +0100

    populated more screen

--- lib/ui/screens/more_screen.dart
+++ lib/ui/screens/more_screen.dart
@@ -1,9 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kjg_muf_app/constants/kjg_colors.dart';
+import 'package:kjg_muf_app/constants/strings.dart';
 import 'package:kjg_muf_app/model/auth_state.dart';
 import 'package:kjg_muf_app/providers/auth_provider.dart';
+import 'package:kjg_muf_app/ui/screens/data_privacy_screen.dart';
 import 'package:kjg_muf_app/ui/screens/game_database_screen.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MoreScreen extends ConsumerWidget {
   const MoreScreen({super.key});
@@ -56,16 +61,80 @@ class MoreScreen extends ConsumerWidget {
           sliver: SliverList(
             delegate: SliverChildListDelegate(
               [
-                ListTile(
-                  title: const Text("Spieledatenbank"),
-                  onTap: () {
-                    Navigator.pop(context);
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const GameDatabase(),
+                Card(
+                  child: Column(
+                    children: [
+                      ListTile(
+                        title: const Text("Spieledatenbank"),
+                        trailing: Icon(
+                          Icons.arrow_forward_ios,
+                          size: 16,
+                        ),
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => const GameDatabase(),
+                            ),
+                          );
+                        },
                       ),
-                    );
-                  },
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 32,
+                ),
+                Card(
+                  child: Column(
+                    children: [
+                      if (!Platform.isIOS) ...[
+                        ListTile(
+                          title: const Text("Website"),
+                          trailing: Icon(
+                            Icons.arrow_forward_ios,
+                            size: 16,
+                          ),
+                          onTap: () async {
+                            await launchUrl(
+                              Uri.parse(Strings.websiteURL),
+                              mode: LaunchMode.externalApplication,
+                            );
+                          },
+                        ),
+                        Divider(height: 0.0),
+                      ],
+                      if (!Platform.isIOS) ...[
+                        ListTile(
+                          title: const Text("Shop"),
+                          trailing: Icon(
+                            Icons.arrow_forward_ios,
+                            size: 16,
+                          ),
+                          onTap: () async {
+                            await launchUrl(
+                              Uri.parse(Strings.shopURL),
+                              mode: LaunchMode.externalApplication,
+                            );
+                          },
+                        ),
+                        Divider(height: 0.0),
+                      ],
+                      ListTile(
+                        title: const Text("Datenschutz"),
+                        trailing: Icon(
+                          Icons.arrow_forward_ios,
+                          size: 16,
+                        ),
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => DataPrivacyScreen(),
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
