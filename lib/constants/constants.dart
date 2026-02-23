78f1c0cb01ca120338b12b3078519b839b5abc36
Fabian Thomas-Barein <fabian@thomas-barein.de>
2026-02-23 19:37:20 +0100

    added userAgent to fix map view

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,4 +1,5 @@
 abstract class Constants {
   static const double phoneMaxWidth = 600;
   static const int maxInt = -1 >>> 1;
+  static const String mapUserAgent = "KjGApp (+https://muenchen.kjg.de)";
 }
